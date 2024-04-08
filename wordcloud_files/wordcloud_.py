4e47fb6ec45e6d1ca3683798407a02ef0bbdab62
Varvara <varvaraparamon@gmail.com>
2024-04-08 18:15:56 +0300

    wc

--- wordcloud_files/wordcloud_.py
+++ wordcloud_files/wordcloud_.py
@@ -1,52 +1,8 @@
 import re
-import pandas as pd
-import os.path
-import psycopg2
-from dotenv import load_dotenv
+
+from db import conn
 
 
-load_dotenv()
-
-#далее изменить на необходимые параметры подключения к бд
-class DB:
-    def __init__(self):
-        self.conn = psycopg2.connect(host='rc1b-2im86q7efcxd3klt.mdb.yandexcloud.net',
-                                     port='6432',
-                                     user='ex_tg',
-                                     password='rFW3sRYyph6xUJw',
-                                     dbname='exhibition_db',
-                                     sslmode='require')
-        self.cur = self.conn.cursor()
-
-    def execute(self, query):
-        self.cur.execute(query)
-
-    def fetch_all(self):
-        return self.cur.fetchall()
-
-    def close(self):
-        self.cur.close()
-        self.conn.close()
-    def commit(self):
-        self.conn.commit()
-
-# conn = DB()
-# query = """select
-# guides.question_answer
-# from public.guides
-# where question_name in (
-#     'answer_short_text_37961013',
-#     'answer_short_text_39100929',
-#     'answer_long_text_37961085',
-#     'answer_short_text_39100908',
-#     'answer_long_text_37961122'
-#     )
-# AND guides.question_answer notnull and guides.question_answer!='' and guides.question_answer!=' '
-# """
-# conn.execute(query)
-# data = conn.fetch_all()
-
-conn = DB()
 query = """select 
 * 
 from (
