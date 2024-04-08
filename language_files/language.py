513709337edd8b438d80154f0250547080e4c732
Varvara <varvaraparamon@gmail.com>
2024-04-08 19:10:35 +0300

    update

--- language_files/language.py
+++ language_files/language.py
@@ -1,4 +1,5 @@
 import re
+from db import conn
 
 #основная функция
 def detect_language(text):
@@ -10,36 +11,6 @@ def detect_language(text):
         return('russian')
 
 
-import os.path
-import psycopg2
-from dotenv import load_dotenv
-
-load_dotenv()
-
-#далее изменить на необходимые параметры подключения к бд
-class DB:
-    def __init__(self):
-        self.conn = psycopg2.connect(host='rc1b-2im86q7efcxd3klt.mdb.yandexcloud.net',
-                                     port='6432',
-                                     user='ex_tg',
-                                     password='rFW3sRYyph6xUJw',
-                                     dbname='exhibition_db',
-                                     sslmode='require')
-        self.cur = self.conn.cursor()
-
-    def execute(self, query):
-        self.cur.execute(query)
-
-    def fetch_all(self):
-        return self.cur.fetchall()
-
-    def close(self):
-        self.cur.close()
-        self.conn.close()
-    def commit(self):
-        self.conn.commit()
-
-conn = DB()
 
 #изменить названия столбцов и таблицы на необходимые
 query = """select 
